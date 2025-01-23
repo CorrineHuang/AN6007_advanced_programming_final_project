c85133355336bd0b36bcf886e7e68e9566865d56
Chunzie96 <chunhan.sim@gmail.com>
2025-01-23 17:06:31 +0800

    Add Utilities file
    Contains the df

---
+++ utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 22:16:42 2025
+
+@author: chunhan
+"""
+import pandas as pd
+
+df=pd.read_csv("/Users/chunhan/Desktop/NTU/BUSINESS ANALYTICS/AN6007 - ADVANCED PROGRAMMING/Group Project/final.csv")
